1c38a02d66db3e2be8788407f229c77270006b0e
Kai Cheng <chengkai@oregonstate.edu>
2024-05-02 22:53:41 -0700

    begin entry field

--- src/components/entryfield.jsx
+++ src/components/entryfield.jsx
@@ -1,8 +1,27 @@
 
 export default function Entryfield(){
-    return(
-        <h1>
-            hello
-        </h1>
-    )
+    return( 
+    <>
+      <div className="expense-input-container">
+        <label for="expense-date" className="expense-label">Date</label>
+        <div className="input-element">
+          <input type="text" name="expense-date" id="expense-input-date" className="expense-input"/> 
+        </div>         
+      </div>
+
+      <div className="expense-input-container">
+        <label for="expense-cost" className="expense-label">Cost</label>
+        <div className="input-element">
+          <input type="number" name="expense-cost" id="expense-input-cost" className="expense-input"/> 
+        </div>         
+
+      </div>
+      <div className="expense-input-container">
+        <label for="expense-description" className="expense-label">Description</label>
+        <div className="input-element">
+          <input type="text" name="expense-description" id="expense-input-description" className="expense-input"/> 
+        </div>         
+      </div>
+    </>
+  )
 }
