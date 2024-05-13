deb66bd2fdf256d5f077798718586109217ee05b
Kai Cheng <chengkai@oregonstate.edu>
2024-05-13 09:47:03 -0700

    start opens both react and electron

--- src/databases/dbManager.js
+++ src/databases/dbManager.js
@@ -1,14 +1,8 @@
 const Database = require("better-sqlite3")
 const path = require("path")
 
-const dbPath =
-    process.env.NODE_ENV === "development"
-        ? "./expenses.db"
-        : path.join(process.resourcesPath, "./expenses.db")
 
-        
-
-const db = new Database(dbPath)
+const db = new Database("./src/databases/expenses.db")
 db.pragma("journal_mode = WAL")
 
 exports.db = db
